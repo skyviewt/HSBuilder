22c3eda5c58b0d8baa47f1f55b374954d6549ead
xiamx <xiamx2004@gmail.com>
2014-12-05 01:25:15 +0000

    boilerplate for accoutn page

--- application/views/account.php
+++ application/views/account.php
@@ -1,12 +1,16 @@
-
 <script type="text/javascript" src="<?=$base_url.$js_path?>account.js"></script>
-<div>
-    <h3>Hi</h3>
-    <h4>Here is a list of you decks</h4>
-    <div class="row" ng-repeat="r in runs">
-        <div class="">
-            
-        </div>
-        
+<div class="container">
+    <div class="greeter">
+        <h2>Welcome {{ username }}</h2>
     </div>
+
+    <div class="row">
+        <div class="col-md-3 runcontainer" ng-repeat="i in [0,1,2,3,4,5,6,7]">
+            <img class="medal" src="/assets/img/NoviceMedal.png"/>
+            <input type="text" class="form-control winloss" placeholder="win">
+            <input type="text" class="form-control winloss" placeholder="loss">
+            <button type="button" class="form-control btn btn-default">Submit</button>
+        </div>
+    </div>
+    
 </div>
